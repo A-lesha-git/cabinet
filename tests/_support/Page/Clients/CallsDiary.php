7a246683c811a2ed55a61982ff524bddc8b57845
gala <agalvis@yandex.ru>
2016-05-30 13:20:13 -0400

    test first form

--- tests/_support/Page/Clients/CallsDiary.php
+++ tests/_support/Page/Clients/CallsDiary.php
@@ -11,17 +11,7 @@ class CallsDiary
     const MEDIUM = '&medium[]=';
     const DURATION_LOWER = '&dur_lower=';
     const DURATION_UPPER = '&dur_upper=';
-    /**
-     * Declare UI map for this page here. CSS or XPath allowed.
-     * public static $usernameField = '#username';
-     * public static $formSubmitButton = "#mainForm input[type=submit]";
-     */
 
-    /**
-     * Basic route example for your current URL
-     * You can append any additional parameter to URL
-     * and use it in tests like: Page\Edit::route('/123-post');
-     */
     
 
     public function __construct(\Codeception\Actor $I)
